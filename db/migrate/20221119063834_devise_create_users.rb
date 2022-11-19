5596dd7dec796d88271bc97db971773b48f636c7
riffiy <117071448+riffiy@users.noreply.github.com>
2022-11-19 09:27:09 +0000

    [add]booker2.3

--- db/migrate/20221119063834_devise_create_users.rb
+++ db/migrate/20221119063834_devise_create_users.rb
@@ -34,13 +34,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
 
 
       t.timestamps null: false
+      t.string :name
+      t.text :introduction
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-    t.string :name
-    t.text :introduction
+   
   end
 end
